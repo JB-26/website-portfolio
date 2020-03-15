9564deeed950897ce892b4954fddbb74a79c5f20
Joshua Blewitt <joshua.blewitt@protonmail.com>
2020-03-15 22:05:37 +0000

    Text changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,6 +16,10 @@ export default () => (
       <h2>Hello! <span role="img" aria-label="string">👋</span></h2>
       <p>My name is Josh (or often called JB) and I'm an aspiring web developer from Milton Keynes, United Kingdom.</p>
       <p>I'm a self taught developer who is passionate about web development. I've had six years of experience in the IT industry. Currently a Software Tester!</p>
+      <h2>About this site</h2>
+      <p>This site was developed in Gatsby (a frameowrk based on React) by myself. I'm constantly learning about web development so the look will change over time. Why not look at my blog to see what I've been learning recently and check out some projects I've made!</p>
+      <h2>Want to contact me?</h2>
+      <p>Why not take a look at the footer for links to my LinkedIn and my Twitter and send me a message? I'm open to junior web development opportunities.</p>
     </div>
     <Footer></Footer>
   </div>
