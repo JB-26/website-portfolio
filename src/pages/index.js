b6058ad2d148749971b5810e67ea8299458b1fff
Joshua Blewitt <joshua.blewitt@protonmail.com>
2020-03-18 22:07:06 +0000

    New CSS file and changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,10 +15,10 @@ export default () => (
     <div className="intro">
       <h2>Hello! <span role="img" aria-label="string">👋</span></h2>
       <p>My name is Josh (or often called JB) and I'm an aspiring web developer from Milton Keynes, United Kingdom.</p>
-      <p>I'm a self taught developer who is passionate about web development. I've had six years of experience in the IT industry. Currently a Software Tester!</p>
-      <h2>About this site</h2>
+      <p>I'm a self taught web developer who is passionate about web development and looking for my first development role. I've had six years of experience in the IT industry. Currently a Software Tester!</p>
+      <h2>About this site <span role="img" aria-label="string">🔍</span></h2>
       <p>This site was developed in Gatsby (a frameowrk based on React) by myself. I'm constantly learning about web development so the look will change over time. Why not look at my blog to see what I've been learning recently and check out some projects I've made!</p>
-      <h2>Want to contact me?</h2>
+      <h2>Want to contact me? <span role="img" aria-label="string">📜</span></h2>
       <p>Why not take a look at the footer for links to my LinkedIn and my Twitter and send me a message? I'm open to junior web development opportunities.</p>
     </div>
     <Footer></Footer>
--- src/pages/work.js
+++ src/pages/work.js
@@ -5,6 +5,10 @@ import Footer from "../components/footer"
 export default () => (
   <div>
     <Header></Header>
+    <div className="titleCard">
+      <h1>Ocean <span role="img" aria-label="string">🌊</span></h1>
+      <hr></hr>
+    </div>
     <Footer></Footer>
   </div>
 )
