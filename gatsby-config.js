e3b3127e72c7dec1c05e9a4c4ca5b0b182dd2f10
Joshua Blewitt <joshua.blewitt@protonmail.com>
2021-04-17 16:00:49 +0100

    Config name change, new button on the homepage!

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `JoshBlewitt.dev`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
