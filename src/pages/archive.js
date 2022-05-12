9dce01caf53bdd79ca4d777ce92b419a748601c4
Joshua Blewitt <joshblewitt@protonmail.com>
2022-05-12 21:47:47 +0100

    Improved CSS and design

--- src/pages/archive.js
+++ src/pages/archive.js
@@ -26,23 +26,16 @@ export default ({ data }) => {
 		{data.allMarkdownRemark.edges.map(({ node }) => (
 		  <div class="container" key={node.id}>
 			<div class="row">
-			  <div class="boxImage">
-				<img src={node.frontmatter.image} class="image"></img>
-			  </div>
-			  <div class="column">
-				<div class="box">
-				  <Link to={node.fields.slug} className="posts">
-					<h3>{node.frontmatter.title}</h3>
-				  </Link>
-				  <hr></hr>
-				</div>
-				<div class="box">
-				  <p id="postSubtitle">{node.frontmatter.description}</p>
-				  <p id="postSubtitle">
-					Published on - {node.frontmatter.date}
-				  </p>
-				</div>
-			  </div>
+			<div class="box-archive">
+			  <Link to={node.fields.slug} className="posts">
+				<h3 id="title">{node.frontmatter.title}</h3>
+			  </Link>
+			</div>
+			<div class="box-archive">
+			  <p id="postSubtitle">
+				Published on - {node.frontmatter.date}
+			  </p>
+			</div>
 			</div>
 		  </div>
 		))}
